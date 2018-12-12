1fd537cc87959833a4e7b2c49ed94a6b42ae34ea
Carlos Carballo <ballokalitos@gmail.com>
2018-12-12 10:55:38 -0500

    Carpeta components heroe heores router2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import { APP_ROUTNG } from "./app.routes";
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeComponent } from './components/heroe.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 @NgModule({
   declarations: [
